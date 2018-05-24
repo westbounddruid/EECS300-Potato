91bd21aea4197505f4e853e1aa73f1c7376eb6f7
westbounddruid <39177016+westbounddruid@users.noreply.github.com>
2018-05-23 22:43:49 -0500

    MDL link for all pages; Add listing touch up

--- browse.js
+++ browse.js
@@ -1,1 +1,34 @@
-//Here goes all our javascript :)
+function searchFunction(event) {
+    var x = event.keyCode;
+    if (x == 13) {
+
+        var input, filter, a, bye_node, c, d, e, z, ex;
+        input = document.getElementById('search-input');
+        filter = input.value.toLowerCase();
+        ex = document.getElementsByClassName("item_card");
+
+        for (i = 0; i < ex.length; i++) {
+            if (ex[i].id == filter) {
+            } else {
+                bye_node = document.getElementById(ex[i].id);
+                bye_node.parentNode.removeChild(bye_node);
+            }
+        }
+        for (i = 0; i < ex.length; i++) {
+            if (ex[i].id == filter) {
+            } else {
+                bye_node = document.getElementById(ex[i].id);
+                bye_node.parentNode.removeChild(bye_node);
+            }
+        }
+        for (i = 0; i < ex.length; i++) {
+            if (ex[i].id == filter) {
+            } else {
+                bye_node = document.getElementById(ex[i].id);
+                bye_node.parentNode.removeChild(bye_node);
+            }
+        }
+    } else {}
+
+}
+;//Here goes all our javascript :)
